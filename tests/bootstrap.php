3efe7dbd8259e5b7812de615ae00ff4809e5d600
Duncan Rumbold <djrumbold@gmail.com>
2017-11-22 16:27:54 +1300

    Add unit test for recently fixed getAssessmentMark function.

---
+++ tests/bootstrap.php
@@ -0,0 +1,11 @@
+<?php
+
+global $databaseConfig;
+if (!isset($databaseConfig)) {
+    $type = $_REQUEST['db']['type'] = defined('SS_DATABASE_CLASS') ? SS_DATABASE_CLASS : 'MySQLDatabase';
+    $databaseConfig = [
+        'type' => $type
+    ];
+}
+
+require_once __DIR__ . '/../framework/tests/bootstrap.php';
