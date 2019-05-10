dcdd5cc29076533f37a926c22e8632a7d39dfe1f
Julian Thomson <julian.thomson@dna.co.nz>
2019-05-10 15:53:30 +1200

    Correct config

--- _config.php
+++ _config.php
@@ -1,9 +1,10 @@
 <?php
 
 use SilverStripe\View\Parsers\ShortcodeParser;
+use DNADesign\Rhino\Pagetypes\RhinoAssessment;
 
 // the current release version. We plan on doing regular releases. This flag is
 // used so we can tell what remote instances are running.
 define('RHINO_VERSION', '4.0');
 
-ShortcodeParser::get('default')->register('assessment_feedback', array('RhinoAssessment', 'assessment_feedback'));
+ShortcodeParser::get('default')->register('assessment_feedback', [RhinoAssessment::class, 'assessment_feedback']);
